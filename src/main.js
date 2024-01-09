793912bf3590cf6b49b502307b6ac46588e1c6af
bao kuanqing <F7691199@idsbg.lh.com>
2024-01-09 17:28:42 +0800

    add a div

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
-import elementUI from 'element-plus'
-import 'element-plus/dist/index.css'
+// import elementUI from 'element-plus'
+// import 'element-plus/dist/index.css'
 
 let app =createApp(App)
-app.use(elementUI)
+// app.use(elementUI)
 app.mount('#app')
