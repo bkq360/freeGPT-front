0f06ad418d232343593805e8f8c722b4021907bd
bao kuanqing <wender_k.q._bao@foxconn.com>
2024-01-16 09:14:52 +0800

    layout main page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,38 @@
 <script setup>
+import { onMounted } from "vue";
+import Button from "./components/button.vue"
+onMounted(()=>{
+  // function test(){
+  //   alert(111)
+  // }
+})
+function test(){
+  let element = document.getElementById("meun-right");
+  let expandButton = document.getElementsByClassName("expand")[0];
+  let width = window.getComputedStyle(element).width;
+  if(element.style.width == "0px"){
+    element.style.width = "14%";
+    expandButton.style.transform = "translateX(50%) translateY(-50%) rotateY(180deg)";
+  }else{
+    element.style.width = "0px";
+    expandButton.style.transform = "translateX(50%) translateY(-50%)";
+  }
+
+  // element.button.style.transform="rotateY(180deg)";
+  // element.style.width = width != "0px" ? "0px" : "14%";
+  
+}
 </script>
 <template>
   <div class="main" > 
     <div class="contain">
-      <div class="tab">
-        
+      <div class="main-content"></div>
+      <div class="main-right" id="meun-right" >
+        test
       </div>
-      <div class="content">
-        
+      <Button @click="test" />
+      <div class="main-left">
+        testa
       </div>
     </div>
   </div>
@@ -19,23 +44,37 @@ body{
   height:100vh;
   overflow: hidden; 
 }
+
 .main{
   width:98%;
-  box-shadow:2px 3px 2px rgba(0, 0, 0, 0.3);
+  box-shadow:2px 3px 2px rgba(0, 0, 0, 0.3),-1px -1px 1px rgba(0,0,0,0.05);
+  border-radius: 6px;
   margin:auto;
   margin-top:15px;
   min-height:calc(100vh - 30px);
-  border:1px solid rgb(196, 193, 193)
+  border:1px solid rgb(196, 193, 193,0.5);
+  overflow: hidden;
 }
 .contain{
   display: flex;
 }
-.tab{
+.main-contain{
+  position: relative;
+  align-items:center
+}
+.main-right{
+  position:relative;
   height:calc(100vh - 30px);
   width:14%;
+  /* left:0; */
+  border-right:1px solid rgb(196, 193, 193,0.5);
+  /* width:0px; */
+  
+  transition: all ease 0.4s;
+  overflow: hidden;
 }
-.content{
+.main-left{
   height:calc(100vh - 30px);
-  width:86%;
+  
 }
 </style>
---
+++ src/components/button.vue
@@ -0,0 +1,35 @@
+<script setup>
+
+</script>
+<template>
+    <div class="expand">
+        <i>
+        <svg viewBox="0 0 16 16" fill="none" opacity="0.6"><path d="M5.64645 3.14645C5.45118 3.34171 5.45118 3.65829 5.64645 3.85355L9.79289 8L5.64645 12.1464C5.45118 12.3417 5.45118 12.6583 5.64645 12.8536C5.84171 13.0488 6.15829 13.0488 6.35355 12.8536L10.8536 8.35355C11.0488 8.15829 11.0488 7.84171 10.8536 7.64645L6.35355 3.14645C6.15829 2.95118 5.84171 2.95118 5.64645 3.14645Z" fill="currentColor"></path></svg>
+        </i>
+    </div>
+</template>
+<style>
+i{
+    font-style: italic;
+}
+.expand{
+    border-radius: 50%;
+    border:1px  solid rgb(196, 193, 193,0.5) ;
+    position:relative;
+    /* position:relative; */
+    /* top: 255px; */
+    margin: auto 0;
+    left: -24px;
+    width: 22px;
+    height:22px;
+    cursor: pointer;
+    background-color:#FFF;
+    /* right:5px; */
+    transform:translateX(50%) translateY(-50%) rotateY(180deg);
+    z-index:1;
+    box-shadow: 0 2px 4px 0px rgba(0,0,0,.06);
+    
+    /* transform: rotateY(180deg); */
+    
+}
+</style>
